f1741d2168890da013042a91fd79574a612d0527
divspark <133004905+divspark@users.noreply.github.com>
2024-11-12 14:15:14 +0530

    Update FavouritePage.jsx

--- src/components/FavouritePage.jsx
+++ src/components/FavouritePage.jsx
@@ -23,7 +23,7 @@ const FavoritePage = () => {
       ) : (
         // Display all favorite movies
         <div className="flex justify-center">
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
             {favorites.map((movie) => (
               <MovieCard key={movie.id} movie={movie} />
             ))}
